58349d1787f983cb469b58db04491d557b123085
Basti Tee <bastis.trash.email@gmx.de>
2014-08-29 18:54:00 +0200

    * fixed broken WSGI module

--- pyntrest_project/wsgi.py
+++ pyntrest_project/wsgi.py
@@ -1,11 +1,13 @@
 """WSGI-hook for Pyntrest to combine Django with a WSGI-compatible web
 server like Apache HTTP or Unicorn."""
 
+# The following two lines MUST stick to the top in that order 
+# right on top. Otherwise WSGI-usage will not work. 
 from os import environ
+environ.setdefault('DJANGO_SETTINGS_MODULE', 'pyntrest_project.settings')
+
 from django.core.wsgi import get_wsgi_application
 from pyntrest.pyntrest_processor import on_startup
-
-environ.setdefault('DJANGO_SETTINGS_MODULE', 'pyntrest_project.settings')
 
 # run Pyntrest's system initialization procedure
 on_startup()

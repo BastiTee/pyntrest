2d16ca096411f59fff7513b2b156f7e32f26ec13
Basti Tee <bastis.trash.email@gmx.de>
2014-08-30 12:45:43 +0200

    more tests for pyntrest_pil

--- pyntrest/pyntrest_pil.py
+++ pyntrest/pyntrest_pil.py
@@ -20,6 +20,9 @@ def rescale_image_dimensions_to_desired_width (width, height, des_width,
     and height and the provided desired image width multiplied by the
     upscale factor"""
     
+    if not width or not height or not des_width or not upscale_factor:
+        raise TypeError
+    
     new_width = des_width
     new_height = int(height * (float(des_width) / float(width)))
     rescaled_width = int(float(new_width) * upscale_factor)
@@ -30,26 +33,36 @@ def create_album_thumbnail_if_not_present (source_image, thumb_image):
     """If the target file does not exist, reads the source file and resizes 
     and crops it with the given width and height"""
     
+    if not source_image:
+        raise TypeError ('source_image not set')
+    if not thumb_image:
+        raise TypeError ('thumb_image not set')
+    if not path.exists(source_image):
+        raise TypeError ('source_image does not exist')
+    if not path.exists(path.dirname(thumb_image)):
+        raise TypeError ('Folder for thumb_image does not exist')
+    
     if not path.exists(thumb_image):
         
-        template_width = int(float(IMAGE_THUMB_WIDTH) * UPSCALE_FACTOR)
+        image = Image.open(source_image)
+        width, height = image.size
+        _, _, res_w, res_h = rescale_image_dimensions_to_desired_width(
+                            width, height, IMAGE_THUMB_WIDTH, UPSCALE_FACTOR)
+        image = image.resize((res_w, res_h), Image.ANTIALIAS)  
         template_height = int(float(IMAGE_THUMB_HEIGHT) * UPSCALE_FACTOR)
         
-        im = Image.open(source_image)
-         
-        width, height = im.size
-        height = int(height * (template_width / float(width)))
-        width = template_width
-        im = im.resize((width, height), Image.ANTIALIAS)
-        left = (width - template_width) / 2
-        upper = (height - template_height) / 2
-        right = width - left
-        lower = height - upper
-        im = im.crop((left, upper, right, lower))
+        left = 0
+        upper = (res_h - template_height) / 2
+        right = res_w
+        lower = res_h - upper
+        image = image.crop((left, upper, right, lower))
         if GIF_PATTERN.match(thumb_image.lower()):
-            im.save(thumb_image, 'GIF')
+            image.save(thumb_image, 'GIF')
         else:
-            im.save(thumb_image, 'JPEG')
+            image.save(thumb_image, 'JPEG')
+    
+    return IMAGE_THUMB_WIDTH, IMAGE_THUMB_HEIGHT
+    
 
 def create_image_thumbnail_if_not_present (source_image, thumb_image):
     """Reads the image width and height from the given image and, if it does 
@@ -68,7 +81,7 @@ def create_image_thumbnail_if_not_present (source_image, thumb_image):
     image = Image.open(source_image)
     width, height = image.size
     new_w, new_h, res_w, res_h = rescale_image_dimensions_to_desired_width(
-                                    width, height, IMAGE_THUMB_WIDTH, UPSCALE_FACTOR )
+                            width, height, IMAGE_THUMB_WIDTH, UPSCALE_FACTOR)
     
     if not path.exists(thumb_image):
         im = image.resize((res_w, res_h), Image.ANTIALIAS) 
--- pyntrest_tests/test_pyntrest_pil.py
+++ pyntrest_tests/test_pyntrest_pil.py
@@ -3,13 +3,39 @@
 from pyntrest import pyntrest_pil
 from PIL import Image        
 from os import path, remove
+import hashlib
 import unittest
 
 class PilTestSuite(unittest.TestCase):
 
     base_path = path.abspath(path.dirname(__file__))
     
-    def test_resize_image ( self ):
+    def md5sum (self, filename):
+        md5 = hashlib.md5()
+        with open(filename, 'rb') as input_file_handle: 
+            for chunk in iter(lambda: input_file_handle.read(
+                            128 * md5.block_size), b''): 
+                md5.update(chunk)
+        return md5.hexdigest()
+    
+    def test_rescale_image_dimensions_to_desired_width (self):
+        self.assertRaises(TypeError, pyntrest_pil.rescale_image_dimensions_to_desired_width)
+        self.assertRaises(TypeError, pyntrest_pil.rescale_image_dimensions_to_desired_width, None)
+        self.assertRaises(TypeError, pyntrest_pil.rescale_image_dimensions_to_desired_width, None, None)
+        self.assertRaises(TypeError, pyntrest_pil.rescale_image_dimensions_to_desired_width, None, None, None)
+        self.assertRaises(TypeError, pyntrest_pil.rescale_image_dimensions_to_desired_width, None, None, None, None)
+        a, b, c, d = pyntrest_pil.rescale_image_dimensions_to_desired_width(500, 400, 200, 1)
+        self.assertEqual(200, a)
+        self.assertEqual(160, b)
+        self.assertEqual(200, c)
+        self.assertEqual(160, d)  
+        a, b, c, d = pyntrest_pil.rescale_image_dimensions_to_desired_width(500, 400, 200, 2)
+        self.assertEqual(200, a)
+        self.assertEqual(160, b)
+        self.assertEqual(400, c)
+        self.assertEqual(320, d)  
+    
+    def test_create_image_thumbnail_if_not_present ( self ):
 
         self.assertRaises(TypeError, pyntrest_pil.create_image_thumbnail_if_not_present)
         self.assertRaises(TypeError, pyntrest_pil.create_image_thumbnail_if_not_present, None)
@@ -25,6 +51,31 @@ class PilTestSuite(unittest.TestCase):
         self.assertEqual(199, height)
         im = Image.open(target_file)
         self.assertEqual((525, 348), im.size)
+        md5 = self.md5sum(target_file)
+        md5_ref =self.md5sum(path.join ( self.base_path, 
+                                         'testdata', 'ref_image-trg.jpg'))
+        self.assertEqual(md5_ref, md5)
+        
+    def test_create_album_thumbnail_if_not_present ( self ):
+
+        self.assertRaises(TypeError, pyntrest_pil.create_album_thumbnail_if_not_present)
+        self.assertRaises(TypeError, pyntrest_pil.create_album_thumbnail_if_not_present, None)
+        self.assertRaises(TypeError, pyntrest_pil.create_album_thumbnail_if_not_present, None, None)
+        self.assertRaises(TypeError, pyntrest_pil.create_album_thumbnail_if_not_present, 
+                          '/somewhere/foo.jpg', '/somewhere/foo2.jpg')
+        source_file = path.join ( self.base_path, 'testdata', 'image.jpg')
+        target_file = path.join ( self.base_path, 'testdata', 'album-trg.jpg')
+        self.addCleanup(remove, target_file)
+        width, height = pyntrest_pil.create_album_thumbnail_if_not_present(source_file, target_file)
+        self.assertTrue(path.exists(target_file))
+        self.assertEqual(300, width)
+        self.assertEqual(150, height)
+        im = Image.open(target_file)
+        self.assertEqual((525, 262), im.size)
+        md5 = self.md5sum(target_file)
+        md5_ref =self.md5sum(path.join ( self.base_path, 
+                                         'testdata', 'ref_album-trg.jpg'))
+        self.assertEqual(md5_ref, md5)
          
 if __name__ == '__main__':
     unittest.main()

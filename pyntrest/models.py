f0522b37641a6ede2b96dec297ee80e8b30b18da
Basti Tee <bastis.trash.email@gmx.de>
2016-01-06 08:45:38 +0100

    Revert "pushed code from weekends work;"
    This reverts commit fc95ccc72ff9066b84fd82c89b412848e56c5781.

--- pyntrest/models.py
+++ pyntrest/models.py
@@ -5,16 +5,13 @@ from django.db import models
 class AlbumImage(models.Model):
     """A single image inside an album"""
     
-    type = models.CharField(max_length=3)
     location =  models.FilePathField()
     title = models.CharField(max_length=200)
     width = models.IntegerField()
     height = models.IntegerField()
+    youtubeid = models.CharField(max_length=20)
     description = models.CharField(max_length=5000)
     modified = models.BooleanField()
-    youtubeid = models.CharField(max_length=20)
-    thumbnail = models.CharField(max_length=1000)
-    divid = models.CharField(max_length=1000)
 
 class Album(models.Model):
     """A web photo album"""

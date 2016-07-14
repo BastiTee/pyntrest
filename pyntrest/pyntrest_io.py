65b832105385f1f2c3b6215393d216e7569124ec
BastiTee <basti.tee@gmx.de>
2016-07-14 21:33:56 +0200

    n

--- pyntrest/pyntrest_io.py
+++ pyntrest/pyntrest_io.py
@@ -3,34 +3,34 @@ of album requests."""
 
 from ConfigParser import ConfigParser
 from re import sub, compile, match, search
-from os import path, makedirs, listdir, walk 
+from os import path, makedirs, listdir, walk
 from time import time
 from codecs import open
 from markdown2 import Markdown
 
 def file_exists (path):
     """Tests if a file exists"""
-    
+
     try:
         fobj = open(path)
         fobj.close()
         return True
     except IOError:
         return False
-    
+
 def mkdirs (directory):
     """Create directory structure if it does not exist"""
-    
+
     if not directory:
         raise TypeError('directory not provided.')
-    
+
     directory_abs = path.abspath(directory)
     if not path.exists(directory_abs):
         makedirs(directory_abs)
 
 def findfiles (absolute_path, filter_regex=None, doprint=False):
     """Lists all files in given directory path recursively."""
-        
+
     filelist = []
     for dirname, _, filenames in walk(absolute_path):
         for filename in filenames:
@@ -39,24 +39,24 @@ def findfiles (absolute_path, filter_regex=None, doprint=False):
                 matching = match(filter_regex, absolute_path.lower())
                 if matching != None:
                     filelist.append(absolute_path)
-                    if (doprint): 
+                    if (doprint):
                         print absolute_path
             else:
                 filelist.append(absolute_path)
-                if (doprint): 
+                if (doprint):
                     print absolute_path
     return filelist
-     
+
 def get_immediate_subdirectories(file_path):
-    """Return the sub-directories of a given file path, but 
+    """Return the sub-directories of a given file path, but
     only the first level."""
-    
+
     if not file_path:
         raise TypeError('file_path not provided.')
-    
+
     directories = []
     for name in listdir(file_path):
-        if (path.isdir(path.join(file_path, name)) 
+        if (path.isdir(path.join(file_path, name))
             and not str(name).startswith('.')):
             directories.append(name)
     directories.sort()
@@ -64,25 +64,25 @@ def get_immediate_subdirectories(file_path):
 
 def cleanup_url_path (url_path):
     """Removes redundant or unwanted symbols from the provided URL path"""
-    
+
     if not url_path:
         url_path = '/'
-    
+
     clean_path = sub ('[/]+', '/', url_path)
     clean_path = sub ('index.html$', '', clean_path)
     return clean_path
 
-def convert_url_path_to_local_filesystem_path (root_filesystem_path, 
+def convert_url_path_to_local_filesystem_path (root_filesystem_path,
                                                  url_path):
-    """Creates a file system sub-folder path from the given URL path and appends 
-    it to the given root filesystem path""" 
-    
+    """Creates a file system sub-folder path from the given URL path and appends
+    it to the given root filesystem path"""
+
     if not root_filesystem_path:
         root_filesystem_path = ''
     if not url_path:
         url_path = ''
     url_path = cleanup_url_path(url_path)
-    
+
     url_path = sub ('^/', '', url_path)
     path_elements = url_path.split('/')
     fs_path_abs = root_filesystem_path
@@ -94,11 +94,11 @@ def convert_url_path_to_local_filesystem_path (root_filesystem_path,
 
 def get_absolute_breadcrumb_filesystem_paths (url_path):
     """Splits up the current URL path and returns a list of path elements"""
-    
+
     if not url_path:
         url_path = ''
     url_path = cleanup_url_path(url_path)
-    
+
     url_path = sub ('^/', '', url_path)
     url_path = sub ('/$', '', url_path)
     subpaths = url_path.split ('/')
@@ -106,11 +106,11 @@ def get_absolute_breadcrumb_filesystem_paths (url_path):
         return subpaths
     else:
         return [''] + subpaths
-    
+
 def read_optional_album_metadata (album_path, filename ):
-    """Checks if a given folder contains an INFO_FILE_NAME and tries to obtain 
+    """Checks if a given folder contains an INFO_FILE_NAME and tries to obtain
     optional album information from a section named 'AlbumInfo'"""
-    
+
     if not album_path:
         album_path = ''
     album_path = path.abspath(album_path)
@@ -118,9 +118,9 @@ def read_optional_album_metadata (album_path, filename ):
         raise TypeError('album_path does not exist!')
     if not filename:
         raise TypeError('filename not provided!')
-    
+
     info_ini_file = path.join(album_path, filename)
-    
+
     # set default values
     album_title = path.basename(album_path)
     album_description = ''
@@ -128,7 +128,7 @@ def read_optional_album_metadata (album_path, filename ):
     album_sorted_rev = False
     album_hide_cover = False
     modified_albums_on_top = False
-    
+
     # try to read file
     if path.exists(info_ini_file):
         config = ConfigParser()
@@ -156,9 +156,9 @@ def read_optional_album_metadata (album_path, filename ):
             album_hide_cover, modified_albums_on_top)
 
 def read_optional_image_metadata (album_path, filename ):
-    """Checks if a given folder contains an INFO_FILE_NAME and tries to obtain 
+    """Checks if a given folder contains an INFO_FILE_NAME and tries to obtain
     optional image information from a section named 'ImageInfo'"""
-    
+
     if not album_path:
         album_path = ''
     album_path = path.abspath(album_path)
@@ -166,12 +166,12 @@ def read_optional_image_metadata (album_path, filename ):
         raise TypeError('album_path does not exist!')
     if not filename:
         raise TypeError('filename not provided!')
-    
+
     info_ini_file = path.join(album_path, filename)
-    
+
     # set default values
     image_infos = {}
-    
+
     # try to read file
     if path.exists(info_ini_file):
         config = ConfigParser()
@@ -182,7 +182,7 @@ def read_optional_image_metadata (album_path, filename ):
                 #print 'Adding detailed info for file {0}'.format(option)
                 info = config.get('ImageInfo', option).strip()
                 image_infos[option.lower()] = info
-        
+
     #print image_infos
     return image_infos
 
@@ -193,9 +193,9 @@ def read_youtube_ini_file ( youtube_file_name ):
         raise TypeError('youtube_file_path not provided.')
     youtube_file_path = path.abspath(youtube_file_name)
     if not path.exists(youtube_file_path):
-        raise TypeError('youtube_file_path does not exist!') 
+        raise TypeError('youtube_file_path does not exist!')
 
-    # first try: extract the YouTube id directly from filename 
+    # first try: extract the YouTube id directly from filename
     # in this case it must be enclosed by % %
     match_obj = search('%([^%]+)%', youtube_file_name)
     if match_obj is not None:
@@ -203,59 +203,59 @@ def read_youtube_ini_file ( youtube_file_name ):
         if compile(".+").match(youtube_id) is not None:
             return youtube_id
 
-    # second try: check file for config section with youtube id     
+    # second try: check file for config section with youtube id
     config = ConfigParser()
     config.readfp(open(youtube_file_path, 'r', 'utf8'))
     if config.has_option('VideoInfo', 'YoutubeId'):
         youtube_id = config.get('VideoInfo', 'YoutubeId')
         return youtube_id
-    
+
     # if not just return empty handed
     return ''
 
 def is_modified ( abs_path, is_file, max_age=48, feature_enabled=False, image_file_pattern=compile('^.*$') ):
     """Check if a file was created between now and now minus the given
     max age in hours. Return false if this feature is not configured."""
-    
+
     if not feature_enabled:
         return False
-    
+
     oldest_epoch = time() - ( max_age * 60.0 * 60.0 )
     is_modified = False
     last_change = 0
-    
-    # on files just check the file .. 
+
+    # on files just check the file ..
     if is_file:
         if path.getctime(abs_path) >= oldest_epoch or path.getmtime(abs_path) >= oldest_epoch:
             is_modified = True
         last_change = max(path.getctime(abs_path), path.getmtime(abs_path))
     # on folders find all images file and check those for changes ( if we would just inspect
-    # the folder we'll get updates, e.g., simply because the folder was touched. 
+    # the folder we'll get updates, e.g., simply because the folder was touched.
     else:
         files = findfiles( abs_path, image_file_pattern, doprint=False)
         for subfile in files:
             if path.getctime(subfile) >= oldest_epoch or path.getmtime(subfile) >= oldest_epoch:
                 is_modified = True
             last_change = max(last_change, path.getctime(abs_path), path.getmtime(abs_path))
-    
+
     return is_modified, last_change
 
 def get_html_content (local_file_path):
     """Converts a local markdown-formatted file to HTML"""
-    
+
     if not local_file_path:
         raise TypeError('local_file_path not provided.')
     if not path.exists(local_file_path):
-        raise TypeError('local_file_path does not exist!') 
-    
+        raise TypeError('local_file_path does not exist!')
+
     with open(local_file_path, 'r') as markdown_file:
         file_content=markdown_file.read()
         markdown_file.close()
-    
+
     markdowner = Markdown()
-    # first textual line is considered as title 
+    # first textual line is considered as title
     title = file_content.strip().split('\n')[0]
-    # the full thing will be converted to HTML 
+    # the full thing will be converted to HTML
     file_content = markdowner.convert(file_content)
 
     return file_content, title
